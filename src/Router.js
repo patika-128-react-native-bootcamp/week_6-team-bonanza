0ae1922758e8cd2c230d3dad599bc05d1d5cc25c
suedacicekli <suedacicekli@gmail.com>
2021-12-25 02:46:41 +0300

    Fix | unneeded api query

--- src/Router.js
+++ src/Router.js
@@ -14,22 +14,6 @@ import Favorites from './pages/Favorites';
 const Tab = createBottomTabNavigator();
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetch = async () => {
-      const result = await axios(
-        //Api Connection
-        `https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=931517162d96f2a2551ae5f33cd51066&hash=${hash}`,
-      );
-      console.log(result.data.data.results); // Get comics
-      setItems(result.data.data.results); // Get comics list
-      setLoading(false); // Loading status
-    };
-    fetch();
-  }, []);
-
   return (
     <NavigationContainer>
       <Tab.Navigator
