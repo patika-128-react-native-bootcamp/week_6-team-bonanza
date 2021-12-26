bb8b60befd4da04e602dd713c5b089ee19f2dfdc
Eray Dereağzı <eray.dereagzi@gmail.com>
2021-12-26 14:49:59 +0300

    Add | Library imports

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './src/Router';
 import {name as appName} from './app.json';
-
+import i18n from './src/languages/i18n';
 AppRegistry.registerComponent(appName, () => App);
